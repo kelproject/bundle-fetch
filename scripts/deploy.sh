ac6ccfc46103946ffacadc00284ac79e3509b22c
Brian Rosner <brosner@eldarion.com>
2016-04-01 15:16:35 -0600

    Deploy to GCS

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
 
-ls bin
-gcloud --version
+here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
+. $here/_env.sh
+
+gsutil cp bin/bundle-fetch gs://release.kelproject.com/binaries/bundle-fetch/bundle-fetch-linux-amd64-$BUILD_TAG
